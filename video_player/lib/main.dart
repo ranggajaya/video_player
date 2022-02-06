c95b4e1e9e10cb2b3cf3a55714aadf934f989c0b
ranggajaya <ranggajaya11@gmail.com>
2022-02-06 22:37:26 +0700

    video_player

---
+++ video_player/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:better_player/better_player.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Video Player"),
+      ),
+
+        body: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: BetterPlayer.network(
+                  'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+                  betterPlayerConfiguration: BetterPlayerConfiguration(
+                  aspectRatio: 16 / 9,
+          ),
+        ),
+      ),
+    );
+  }
+}
